c33d0adfe51bd37d49220f159ecd01fae1b03579
YusifH16 <yharhara180@gmail.com>
2025-11-22 20:23:20 -0500

    Fixed stuff

--- List.hpp
+++ List.hpp
@@ -1,186 +1,233 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-#include <iostream>
-#include <iterator>
 #include <cassert>
 
 template <typename T>
-class List {
+class List
+{
+private:
+  struct Node
+  {
+    Node *next;
+    Node *prev;
+    T datum;
+    Node(const T &d, Node *p, Node *n)
+        : next(n), prev(p), datum(d) {}
+  };
+
+  Node *first;
+  Node *last;
+  int sz;
+
+  void copy_all(const List &other)
+  {
+    for (Node *cur = other.first; cur; cur = cur->next)
+    {
+      push_back(cur->datum);
+    }
+  }
+
 public:
-  bool empty() const {
+  class Iterator
+  {
+  private:
+    const List *list_ptr;
+    Node *node_ptr;
+
+    Iterator(const List *lp, Node *np)
+        : list_ptr(lp), node_ptr(np) {}
+
+    friend class List;
+
+  public:
+    Iterator() : list_ptr(nullptr), node_ptr(nullptr) {}
+
+    T &operator*() const
+    {
+      return node_ptr->datum;
+    }
+
+    Iterator &operator++()
+    {
+      if (node_ptr)
+        node_ptr = node_ptr->next;
+      return *this;
+    }
+
+    Iterator operator++(int)
+    {
+      Iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    bool operator==(const Iterator &rhs) const
+    {
+      return list_ptr == rhs.list_ptr && node_ptr == rhs.node_ptr;
+    }
+
+    bool operator!=(const Iterator &rhs) const
+    {
+      return !(*this == rhs);
+    }
+  };
+
+  List() : first(nullptr), last(nullptr), sz(0) {}
+
+  ~List()
+  {
+    clear();
+  }
+
+  List(const List &other)
+      : first(nullptr), last(nullptr), sz(0)
+  {
+    copy_all(other);
+  }
+
+  List &operator=(const List &other)
+  {
+    if (this != &other)
+    {
+      clear();
+      copy_all(other);
+    }
+    return *this;
+  }
+
+  bool empty() const
+  {
     return first == nullptr;
   }
 
-  int size() const {
+  int size() const
+  {
     return sz;
   }
 
-  T& front() {
+  T &front()
+  {
     return first->datum;
   }
 
-  T& back() {
+  T &back()
+  {
     return last->datum;
   }
 
-  void push_front(const T& datum) {
-    Node* n = new Node{first, nullptr, datum};
-    if (first) first->prev = n;
+  void push_front(const T &datum)
+  {
+    Node *n = new Node(datum, nullptr, first);
+    if (first)
+      first->prev = n;
     first = n;
-    if (!last) last = n;
-    sz++;
+    if (!last)
+      last = n;
+    ++sz;
   }
 
-  void push_back(const T& datum) {
-    Node* n = new Node{nullptr, last, datum};
-    if (last) last->next = n;
+  void push_back(const T &datum)
+  {
+    Node *n = new Node(datum, last, nullptr);
+    if (last)
+      last->next = n;
     last = n;
-    if (!first) first = n;
-    sz++;
+    if (!first)
+      first = n;
+    ++sz;
   }
 
-  void pop_front() {
-    Node* tmp = first;
+  void pop_front()
+  {
+    Node *tmp = first;
     first = first->next;
-    if (first) first->prev = nullptr;
-    else last = nullptr;
+    if (first)
+      first->prev = nullptr;
+    else
+      last = nullptr;
     delete tmp;
-    sz--;
+    --sz;
   }
 
-  void pop_back() {
-    Node* tmp = last;
+  void pop_back()
+  {
+    Node *tmp = last;
     last = last->prev;
-    if (last) last->next = nullptr;
-    else first = nullptr;
+    if (last)
+      last->next = nullptr;
+    else
+      first = nullptr;
     delete tmp;
-    sz--;
+    --sz;
   }
 
-  void clear() {
-    Node* cur = first;
-    while (cur) {
-      Node* nxt = cur->next;
+  void clear()
+  {
+    Node *cur = first;
+    while (cur)
+    {
+      Node *next = cur->next;
       delete cur;
-      cur = nxt;
+      cur = next;
     }
     first = nullptr;
     last = nullptr;
     sz = 0;
   }
 
-  List() : first(nullptr), last(nullptr), sz(0) {}
-
-  ~List() {
-    clear();
-  }
-
-  List(const List& other) : first(nullptr), last(nullptr), sz(0) {
-    copy_all(other);
-  }
-
-  List& operator=(const List& other) {
-    if (this == &other) return *this;
-    clear();
-    copy_all(other);
-    return *this;
-  }
-
-private:
-  struct Node {
-    Node* next;
-    Node* prev;
-    T datum;
-  };
-
-  void copy_all(const List<T>& other) {
-    for (Node* cur = other.first; cur; cur = cur->next) {
-      push_back(cur->datum);
-    }
-  }
-
-  Node* first;
-  Node* last;
-  int sz;
-
-public:
-  class Iterator {
-  public:
-    Iterator() : list_ptr(nullptr), node_ptr(nullptr) {}
-
-    T& operator*() const {
-      return node_ptr->datum;
-    }
-
-    Iterator& operator++() {
-      if (node_ptr) node_ptr = node_ptr->next;
-      return *this;
-    }
-
-    Iterator operator++(int) {
-      Iterator tmp = *this;
-      operator++();
-      return tmp;
-    }
-
-    bool operator==(const Iterator& rhs) const {
-      return list_ptr == rhs.list_ptr && node_ptr == rhs.node_ptr;
-    }
-
-    bool operator!=(const Iterator& rhs) const {
-      return !(*this == rhs);
-    }
-
-  private:
-    const List* list_ptr;
-    Node* node_ptr;
-
-    Iterator(const List* lp, Node* np) : list_ptr(lp), node_ptr(np) {}
-
-    friend class List;
-  };
-
-  Iterator begin() const {
+  Iterator begin() const
+  {
     return Iterator(this, first);
   }
 
-  Iterator end() const {
+  Iterator end() const
+  {
     return Iterator(this, nullptr);
   }
 
-  Iterator erase(Iterator i) {
-    Node* n = i.node_ptr;
-    Node* nxt = n->next;
-    Node* prv = n->prev;
+  Iterator erase(Iterator it)
+  {
+    Node *n = it.node_ptr;
+    Node *nxt = n->next;
+    Node *prv = n->prev;
 
-    if (n == first) first = nxt;
-    if (n == last) last = prv;
+    if (n == first)
+      first = nxt;
+    if (n == last)
+      last = prv;
 
-    if (prv) prv->next = nxt;
-    if (nxt) nxt->prev = prv;
+    if (prv)
+      prv->next = nxt;
+    if (nxt)
+      nxt->prev = prv;
 
     delete n;
-    sz--;
+    --sz;
 
     return Iterator(this, nxt);
   }
 
-  Iterator insert(Iterator i, const T& datum) {
-    Node* pos = i.node_ptr;
-    if (!pos) {
+  Iterator insert(Iterator it, const T &datum)
+  {
+    Node *pos = it.node_ptr;
+
+    if (!pos)
+    {
       push_back(datum);
       return Iterator(this, last);
     }
 
-    Node* prv = pos->prev;
-    Node* n = new Node{pos, prv, datum};
+    Node *prv = pos->prev;
+    Node *n = new Node(datum, prv, pos);
 
     pos->prev = n;
-    if (prv) prv->next = n;
-    else first = n;
+    if (prv)
+      prv->next = n;
+    else
+      first = n;
 
-    sz++;
+    ++sz;
     return Iterator(this, n);
   }
 };
--- TextBuffer.cpp
+++ TextBuffer.cpp
@@ -1,191 +1,213 @@
 #include "TextBuffer.hpp"
 
 TextBuffer::TextBuffer()
-  : data(), cursor(data.end()), row(1), column(0), index(0) {}
+    : data(),
+      cursor(data.end()),
+      row(1),
+      column(0),
+      index(0)
+{
+}
 
-bool TextBuffer::forward() {
-  if (cursor == data.end()) return false;
+bool TextBuffer::forward()
+{
+  if (cursor == data.end())
+    return false;
   char c = *cursor;
   ++cursor;
-  index++;
-  if (c == '\n') {
-    row++;
+  ++index;
+  if (c == '\n')
+  {
+    ++row;
     column = 0;
-  } else {
-    column++;
+  }
+  else
+  {
+    ++column;
   }
   return true;
 }
 
-bool TextBuffer::backward() {
-  if (cursor == data.begin()) return false;
+bool TextBuffer::backward()
+{
+  if (cursor == data.begin())
+    return false;
   --cursor;
-  index--;
-  if (*cursor == '\n') {
-    row--;
+  --index;
+  if (*cursor == '\n')
+  {
+    --row;
     column = compute_column();
-  } else {
-    column--;
+  }
+  else
+  {
+    --column;
   }
   return true;
 }
 
-void TextBuffer::insert(char c) {
+void TextBuffer::insert(char c)
+{
   cursor = data.insert(cursor, c);
-  if (cursor != data.end()) ++cursor;
-  if (cursor == data.end()) {}
-  if (index == 0) {}
-  if (cursor == data.end()) index++;
-  else index = index;
-  if (cursor == data.end() && data.size() == 1) {
-    column = 1;
+  ++cursor;
+  ++index;
+  if (c == '\n')
+  {
+    ++row;
+    column = 0;
   }
-  if (cursor != data.end()) {
-    if (c == '\n') {
-      row++;
-      column = 0;
-    } else {
-      column++;
-    }
+  else
+  {
+    ++column;
   }
-  if (cursor == data.end()) {}
 }
 
-bool TextBuffer::remove() {
-  if (cursor == data.end()) return false;
+bool TextBuffer::remove()
+{
+  if (cursor == data.end())
+    return false;
   char c = *cursor;
-  auto next = cursor;
-  ++next;
-  cursor = data.erase(cursor);
-  if (c == '\n') {
+  auto next = data.erase(cursor);
+  cursor = next;
+  index = (cursor == data.end() ? data.size() : index);
+  if (c == '\n')
     column = compute_column();
-  }
-  if (cursor == data.end()) {
-    index = data.size();
-    return true;
-  }
-  index = index;
   return true;
 }
 
-void TextBuffer::move_to_row_start() {
-  while (cursor != data.begin()) {
+void TextBuffer::move_to_row_start()
+{
+  while (cursor != data.begin())
+  {
     auto prev = cursor;
     --prev;
-    if (*prev == '\n') break;
+    if (*prev == '\n')
+      break;
     backward();
   }
   column = 0;
   index = get_index();
 }
 
-void TextBuffer::move_to_row_end() {
+void TextBuffer::move_to_row_end()
+{
   auto it = cursor;
-  while (it != data.end() && *it != '\n') ++it;
+  while (it != data.end() && *it != '\n')
+    ++it;
   cursor = it;
   index = get_index();
-  if (cursor == data.end()) {
-    column = compute_column();
-  } else {
-    column = compute_column();
-  }
+  column = compute_column();
 }
 
-void TextBuffer::move_to_column(int new_column) {
+void TextBuffer::move_to_column(int new_column)
+{
   move_to_row_start();
   int count = 0;
-  while (cursor != data.end() && *cursor != '\n' && count < new_column) {
+  while (cursor != data.end() && *cursor != '\n' && count < new_column)
+  {
     forward();
-    count++;
+    ++count;
   }
 }
 
-bool TextBuffer::up() {
-  if (row == 1) return false;
+bool TextBuffer::up()
+{
+  if (row == 1)
+    return false;
   int target = column;
   move_to_row_start();
-  bool moved = backward();
-  while (cursor != data.begin() && *cursor != '\n') backward();
-  if (cursor != data.begin()) backward();
-  int c = target;
-  while (cursor != data.end() && *cursor != '\n' && c > 0) {
+  if (!backward())
+    return false;
+  while (cursor != data.begin() && *cursor != '\n')
+    backward();
+  if (*cursor == '\n')
     forward();
-    c--;
+  int c = target;
+  while (cursor != data.end() && *cursor != '\n' && c > 0)
+  {
+    forward();
+    --c;
   }
   return true;
 }
 
-bool TextBuffer::down() {
+bool TextBuffer::down()
+{
   auto it = cursor;
-  while (it != data.end() && *it != '\n') ++it;
-  if (it == data.end()) return false;
+  while (it != data.end() && *it != '\n')
+    ++it;
+  if (it == data.end())
+    return false;
   ++it;
-  if (it == data.end()) return false;
+  if (it == data.end())
+    return false;
   int target = column;
   cursor = it;
-  row++;
+  ++row;
   column = 0;
   index = get_index();
-  while (cursor != data.end() && *cursor != '\n' && target > 0) {
+  while (cursor != data.end() && *cursor != '\n' && target > 0)
+  {
     forward();
-    target--;
+    --target;
   }
   return true;
 }
 
-bool TextBuffer::is_at_end() const {
+bool TextBuffer::is_at_end() const
+{
   return cursor == data.end();
 }
 
-char TextBuffer::data_at_cursor() const {
+char TextBuffer::data_at_cursor() const
+{
   return *cursor;
 }
 
-int TextBuffer::get_row() const {
-  return row;
-}
+int TextBuffer::get_row() const { return row; }
+int TextBuffer::get_column() const { return column; }
+int TextBuffer::get_index() const { return index; }
+int TextBuffer::size() const { return data.size(); }
 
-int TextBuffer::get_column() const {
-  return column;
-}
-
-int TextBuffer::get_index() const {
-  return index;
-}
-
-int TextBuffer::size() const {
-  return data.size();
-}
-
-std::string TextBuffer::stringify() const {
+std::string TextBuffer::stringify() const
+{
   return std::string(data.begin(), data.end());
 }
 
-int TextBuffer::compute_column() const {
+int TextBuffer::compute_column() const
+{
   int col = 0;
   auto it = cursor;
-  if (it == data.end()) {
-    if (!data.empty()) {
-      it = data.end();
-      --it;
-      if (*it == '\n') return 0;
-      while (it != data.begin()) {
-        auto prev = it;
-        --prev;
-        if (*prev == '\n') break;
-        it = prev;
-        col++;
-      }
-      return col + 1;
+
+  if (it == data.end())
+  {
+    if (data.empty())
+      return 0;
+    it = data.end();
+    --it;
+    if (*it == '\n')
+      return 0;
+    while (it != data.begin())
+    {
+      auto prev = it;
+      --prev;
+      if (*prev == '\n')
+        break;
+      ++col;
+      it = prev;
     }
-    return 0;
+    return col + 1;
   }
-  while (it != data.begin()) {
+
+  while (it != data.begin())
+  {
     auto prev = it;
     --prev;
-    if (*prev == '\n') break;
+    if (*prev == '\n')
+      break;
+    ++col;
     it = prev;
-    col++;
   }
+
   return col;
 }
